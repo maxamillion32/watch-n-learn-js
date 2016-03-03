a465cdcad44124f675a93e49c0eba62f13b403d7
gangachris <ganga.chris@gmail.com>
2016-03-03 11:18:05 +0300

    (chore) Initial Working Angular Setup

---
+++ public/app/main.ts
@@ -0,0 +1,11 @@
+import {Component} from 'angular2/core';
+import {bootstrap} from 'angular2/platform/browser';
+
+@Component({
+  selector: 'my-app',
+  template: '<h1>Hello</h1>'
+})
+
+class AppComponent {}
+
+bootstrap(AppComponent);
