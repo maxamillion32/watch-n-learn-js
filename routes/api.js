6bcff7796c533a5e60f9040a561aa349cf531c1f
gangachris <ganga.chris@gmail.com>
2016-03-07 15:27:45 +0300

    Add users post and get routes

--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,44 @@
 var express = require('express');
 var router = express.Router();
+var db = require('../db');
+var faker = require('faker');
 
 /* GET users listing. */
 router.get('/', function (req, res, next) {
-  res.send('Hello');
+  res.send('API Root');
+});
+
+router.get('/users', function (req, res) {
+  db
+    .select()
+    .from('users')
+    .then(function(users) {
+        res.status(200)
+            .json(users);
+    })
+    .catch(function(error) {
+        res.status(500)
+            .send(error);
+    });
+});
+
+router.post('/users', function(req, res) {
+   var user = req.body;
+   db
+    .insert(user)
+    .into('users')
+    .then(function(result) {
+       res
+        .status(201)
+        .json({
+            message: 'User successfully created'
+        });
+   })
+   .catch(function(error) {
+       res
+        .status(400)
+        .json(error);
+   });
 });
 
 module.exports = router
