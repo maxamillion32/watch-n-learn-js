8bc12a3818471957510513194f44214c5894a046
gangachris <ganga.chris@gmail.com>
2016-03-08 15:41:00 +0300

    Import RxJS

--- public/app/main.ts
+++ public/app/main.ts
@@ -1,4 +1,7 @@
+import 'rxjs/Rx';
+
 import {bootstrap} from 'angular2/platform/browser';
+import {ROUTER_PROVIDERS} from 'angular2/router';
 
 import {AppComponent} from './app.component/app.component';
 
