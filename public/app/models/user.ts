c55c0aceb29c9b68783745ef17f0c586e1e71ba6
gangachris <ganga.chris@gmail.com>
2016-03-08 15:41:19 +0300

    Add user model

---
+++ public/app/models/user.ts
@@ -0,0 +1,13 @@
+export class User {
+    public id: number;
+    public name: string;
+    public username: string;
+    public email: string;
+    public password: string;
+    public avatar: string;
+    public bio: string;
+    public provider: string;
+    public uid: string;
+    public created_at: string;
+    public updated_at: string;
+}
