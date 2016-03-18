2b38a77a6a7599815a3f89ea302d8f849e658a27
gangachris <ganga.chris@gmail.com>
2016-03-18 17:00:10 +0300

    Add Auth Service

--- client/app/app.component/app.component.js
+++ client/app/app.component/app.component.js
@@ -1,4 +1,4 @@
-System.register(['angular2/core', 'angular2/router', 'angular2/http', '../home/home.component', '../auth/register/register.component', '../auth/login/login.component'], function(exports_1, context_1) {
+System.register(['angular2/core', 'angular2/router', 'angular2/http', '../auth/auth.service', '../home/home.component', '../auth/register/register.component', '../auth/login/login.component'], function(exports_1, context_1) {
     "use strict";
     var __moduleName = context_1 && context_1.id;
     var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
@@ -10,7 +10,7 @@ System.register(['angular2/core', 'angular2/router', 'angular2/http', '../home/h
     var __metadata = (this && this.__metadata) || function (k, v) {
         if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
     };
-    var core_1, router_1, http_1, home_component_1, register_component_1, login_component_1;
+    var core_1, router_1, http_1, auth_service_1, home_component_1, register_component_1, login_component_1;
     var AppComponent;
     return {
         setters:[
@@ -22,6 +22,9 @@ System.register(['angular2/core', 'angular2/router', 'angular2/http', '../home/h
             },
             function (http_1_1) {
                 http_1 = http_1_1;
+            },
+            function (auth_service_1_1) {
+                auth_service_1 = auth_service_1_1;
             },
             function (home_component_1_1) {
                 home_component_1 = home_component_1_1;
@@ -46,7 +49,8 @@ System.register(['angular2/core', 'angular2/router', 'angular2/http', '../home/h
                         ],
                         providers: [
                             router_1.ROUTER_PROVIDERS,
-                            http_1.HTTP_PROVIDERS
+                            http_1.HTTP_PROVIDERS,
+                            auth_service_1.AuthService
                         ]
                     }),
                     router_1.RouteConfig([
