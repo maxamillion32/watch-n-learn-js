a10eaa973d8aaad6b94327b88415f609dd753e51
gangachris <ganga.chris@gmail.com>
2016-03-31 10:58:18 +0300

    Add stylesUrl

--- client/app/home/home.component.ts
+++ client/app/home/home.component.ts
@@ -2,7 +2,8 @@ import {Component} from 'angular2/core'
 
 @Component({
     selector: 'home',
-    templateUrl: './app/home/home.compnent.html'
+    templateUrl: './app/home/home.compnent.html',
+    styleUrls: ['./app/home/home.component.css']
 })
 
-export class HomeComponent{}
+export class HomeComponent {}
