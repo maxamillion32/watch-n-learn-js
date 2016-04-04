902a21ce9ba813dd3b359c4083a8698e80667161
gangachris <ganga.chris@gmail.com>
2016-04-04 16:31:02 +0300

    Remove unimplemented files

--- client/app/profile/profile.component.ts
+++
@@ -1,8 +0,0 @@
-import {Component} from 'angular2/core'
-
-@Component({
-    selector: 'profile',
-    template: '<h1>Profile</h1>'
-})
-
-export class ProfileComponent {}
--- client/app/project/project.component.ts
+++
@@ -1,8 +0,0 @@
-import {Component} from 'angular2/core'
-
-@Component({
-    selector: 'project',
-    template: './app/project/project.component.html',
-    styleUrls: ['./app/project/project.component.css']
-})
-export class ProjectComponent{}
