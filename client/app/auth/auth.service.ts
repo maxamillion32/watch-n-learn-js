d9f49dcf5a21848b81a6fd2ae19b0230ddd82777
gangachris <ganga.chris@gmail.com>
2016-04-09 10:08:45 +0300

    fix tslint

--- client/app/auth/auth.service.ts
+++ client/app/auth/auth.service.ts
@@ -4,9 +4,8 @@ import {Observable} from 'rxjs/Observable';
 
 @Injectable()
 export class AuthService {
-    
-    constructor(private _http: Http){}
-    
+    constructor(private _http: Http) {}
+
     AuthUser() {
         if (localStorage.getItem('wnljwt')) {
             return localStorage.getItem('wnluser');
@@ -14,24 +13,24 @@ export class AuthService {
             return false;
         }
     }
-    
+
     createUser(user) {
         let body = JSON.stringify(user);
         let headers = new Headers({'Content-Type': 'application/json'});
         let options = new RequestOptions({headers: headers});
-        
+
         return this._http
             .post('/api/users', body, options)
             .map(res => res.json())
             .do(res => console.log(res)) // comment out in production
-            .catch(this.handleError);                    
+            .catch(this.handleError);
     }
-    
+
     login(user) {
         let body = JSON.stringify(user);
         let headers = new Headers({'Content-Type': 'application/json'});
         let options = new RequestOptions({headers: headers});
-        
+
         return this._http
                 .post('/login', body, options)
                 .map(res => {
@@ -40,10 +39,10 @@ export class AuthService {
                     }
                     return res.json();
                 })
-                .do(res => console.log(res)) //comment out in production
+                .do(res => console.log(res)) // comment out in production
                 .catch(this.handleError);
     }
-    
+
     private handleError(error: Response) {
         console.log(error);
         return Observable.throw(error.json().error || 'Server error');
