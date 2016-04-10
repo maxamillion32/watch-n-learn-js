3d7927aff69d5ea3c64e8f4d819fc24b7e298f28
gangachris <ganga.chris@gmail.com>
2016-04-10 10:10:04 +0300

    Fix tslint

--- client/app/home/home.component.ts
+++ client/app/home/home.component.ts
@@ -1,9 +1,9 @@
-import {Component, OnInit} from "angular2/core";
+import {Component, OnInit} from 'angular2/core';
 
 @Component({
-    selector: "home",
-    templateUrl: "./app/home/home.compnent.html",
-    styleUrls: ["./app/home/home.component.css"]
+    selector: 'home',
+    templateUrl: './app/home/home.compnent.html',
+    styleUrls: ['./app/home/home.component.css']
 })
 
 export class HomeComponent implements OnInit {
