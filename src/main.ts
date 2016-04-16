83fc5a070c12a44fd88aa67de9e42758e15020c5
gangachris <ganga.chris@gmail.com>
2016-04-16 09:35:49 +0300

    (chore) Format imports

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,8 @@ import { bootstrap } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
 import {HTTP_PROVIDERS} from '@angular/http';
 
-import { FIREBASE_PROVIDERS,
+import { 
+  FIREBASE_PROVIDERS,
   defaultFirebase,
   AuthMethods,
   AuthProviders,
