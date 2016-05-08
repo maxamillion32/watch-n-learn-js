aad97618f8b9dac4b58ce63d1768654f03bfa538
gangachris <ganga.chris@gmail.com>
2016-05-08 08:55:22 +0300

    set up firebase

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
 import { bootstrap } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
 import { WatchNLearnAppComponent, environment } from './app/';
+import { FIREBASE_PROVIDERS, defaultFirebase } from 'angularfire2';
 
 if (environment.production) {
   enableProdMode();
 }
 
-bootstrap(WatchNLearnAppComponent);
+bootstrap(WatchNLearnAppComponent, [
+  FIREBASE_PROVIDERS,
+  defaultFirebase('https://watch-n-learn.firebaseio.com/')
+]);
