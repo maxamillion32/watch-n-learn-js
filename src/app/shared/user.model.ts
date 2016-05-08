7090476176c38539f6d5977c060d7e1740c0462d
gangachris <ganga.chris@gmail.com>
2016-05-08 13:51:14 +0300

    Add authentication model

---
+++ src/app/shared/user.model.ts
@@ -0,0 +1,7 @@
+export class User {
+  id: string;
+  name: string;
+  username: string;
+  avatar: string;
+  email: string;
+}
--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,28 @@
 import { bootstrap } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
+import {HTTP_PROVIDERS} from '@angular/http';
+
+import { FIREBASE_PROVIDERS,
+  defaultFirebase,
+  AuthMethods,
+  AuthProviders,
+  firebaseAuthConfig } from 'angularfire2';
+
+import { MdIconRegistry } from '@angular2-material/icon';
+
 import { WatchNLearnAppComponent, environment } from './app/';
-import { FIREBASE_PROVIDERS, defaultFirebase } from 'angularfire2';
 
 if (environment.production) {
   enableProdMode();
 }
 
 bootstrap(WatchNLearnAppComponent, [
+  HTTP_PROVIDERS,
   FIREBASE_PROVIDERS,
-  defaultFirebase('https://watch-n-learn.firebaseio.com/')
+  MdIconRegistry,
+  defaultFirebase('https://watch-n-learn.firebaseio.com/'),
+  firebaseAuthConfig({
+    provider: AuthProviders.Github,
+    method: AuthMethods.Redirect
+  })
 ]);
